d7983b713bc19f298a5f599a3e44b80b6c9e2088
Sanyamgoyal21 <sanyamgoyal21@gmail.com>
2025-04-11 22:02:58 +0530

    priceHistoryHelpor Change

--- Backend/helpers/priceHistoryHelper.js
+++ Backend/helpers/priceHistoryHelper.js
@@ -10,7 +10,7 @@ exports.getPriceHistory = async (req, res) => {
     }
 };
 
-// Add a price record
+
 exports.addPriceRecord = async (req, res) => {
     try {
         const newPriceRecord = new PriceHistory(req.body);
