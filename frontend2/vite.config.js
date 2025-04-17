874aea624fdd15bdbae928a5e17b8dfd2b1bb76e
Sanyamgoyal21 <146330589+Sanyamgoyal21@users.noreply.github.com>
2025-04-17 18:30:40 +0530

    Update vite.config.js

--- frontend2/vite.config.js
+++ frontend2/vite.config.js
@@ -1,7 +1,13 @@
+// Importing defineConfig helper from Vite to get better type hints and autocompletion
 import { defineConfig } from 'vite'
+// Importing the React plugin to enable support for React features like JSX
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
+// Exporting the Vite configuration
 export default defineConfig({
-  plugins: [react()],
+  // Registering plugins used by Vite
+  plugins: [
+    react()// Enables React fast refresh, JSX transformation, etc.
+  ],
 })
